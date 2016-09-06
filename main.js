40ee03dd9c1b3770aed95e9c56c7812b9837151d
Peter Raswono <peter@mulmut.com>
2016-09-07 01:59:42 +0700

    -Update CSS on laptop

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 console.log('Hello Hacker! If you\'re reading this, you should join us!')
 
 // Append the Templates
-var templateWinner = Tangular.compile(document.getElementById('template-winner').innerHTML);
-$( ".winners .container" ).append(templateWinner(data));
+// var templateWinner = Tangular.compile(document.getElementById('template-winner').innerHTML);
+// $( ".winners .container" ).append(templateWinner(data));
 
-var templateChallenge = Tangular.compile(document.getElementById('template-challenge').innerHTML);
-$( ".challenges .container" ).append(templateChallenge(data));
+// var templateChallenge = Tangular.compile(document.getElementById('template-challenge').innerHTML);
+// $( ".challenges .container" ).append(templateChallenge(data));
 
 var templateMentor = Tangular.compile(document.getElementById('template-mentor').innerHTML);
 $( ".mentor .container" ).append(templateMentor(data));
 
-var templateJudge = Tangular.compile(document.getElementById('template-judge').innerHTML);
-$( ".judge .container" ).append(templateJudge(data));
+// var templateJudge = Tangular.compile(document.getElementById('template-judge').innerHTML);
+// $( ".judge .container" ).append(templateJudge(data));
 
-var templateOrganizer = Tangular.compile(document.getElementById('template-organizer').innerHTML);
-$( ".organizer .container" ).append(templateOrganizer(data));
+// var templateOrganizer = Tangular.compile(document.getElementById('template-organizer').innerHTML);
+// $( ".organizer .container" ).append(templateOrganizer(data));
 
 var templateSponsor = Tangular.compile(document.getElementById('template-sponsors').innerHTML);
 $( ".sponsors .container" ).append(templateSponsor(data));
