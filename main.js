7e847762b1e0d4e021d778a5ae34c93ff17fe2df
Peter Raswono <peter@mulmut.com>
2016-09-11 16:11:20 +0700

    -Update CSS iPad phone laptop

--- main.js
+++ main.js
@@ -7,14 +7,14 @@ console.log('Hello Hacker! If you\'re reading this, you should join us!')
 // var templateChallenge = Tangular.compile(document.getElementById('template-challenge').innerHTML);
 // $( ".challenges .container" ).append(templateChallenge(data));
 
-var templateMentor = Tangular.compile(document.getElementById('template-mentor').innerHTML);
-$( ".mentor .container" ).append(templateMentor(data));
+var templateMentor = Tangular.compile(document.getElementById('template-mentors').innerHTML);
+$( ".mentors .container" ).append(templateMentor(data));
 
 // var templateJudge = Tangular.compile(document.getElementById('template-judge').innerHTML);
 // $( ".judge .container" ).append(templateJudge(data));
 
-// var templateOrganizer = Tangular.compile(document.getElementById('template-organizer').innerHTML);
-// $( ".organizer .container" ).append(templateOrganizer(data));
+var templateOrganizer = Tangular.compile(document.getElementById('template-organizers').innerHTML);
+$( ".organizers .container" ).append(templateOrganizer(data));
 
 var templateSponsor = Tangular.compile(document.getElementById('template-sponsors').innerHTML);
 $( ".sponsors .container" ).append(templateSponsor(data));
@@ -22,8 +22,8 @@ $( ".sponsors .container" ).append(templateSponsor(data));
 var templatePartner = Tangular.compile(document.getElementById('template-partners').innerHTML);
 $( ".partners .container" ).append(templatePartner(data));
 
-var templateApi = Tangular.compile(document.getElementById('template-api').innerHTML);
-$( ".api .container" ).append(templateApi(data));
+// var templateApi = Tangular.compile(document.getElementById('template-api').innerHTML);
+// $( ".api .container" ).append(templateApi(data));
 
 var templateFaq = Tangular.compile(document.getElementById('template-faq').innerHTML);
 $( ".faq .row" ).append(templateFaq(data));
